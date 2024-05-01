1418998872a1a431a6ac3ee75f98ba15c403b4f6
mhamzaaamir960 <mhamzaaamir960.gmail.com>
2024-05-01 12:12:26 +0500

    updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,14 @@
-import Head from 'next/head'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404 - Not Found",
+};
+
 function NotFoundPage() {
   return (
     <div className="min-h-[85vh] flex flex-col justify-center items-center">
-      <Head>
-        <title>404 - Not Found</title>
-      </Head>
-      <h1 className='text-secondary font-bold text-3xl sm:text-4xl'>404</h1>
-      <p className=' text-xl sm:text-2xl'>Error! Page not found...</p>
+      <h1 className="text-secondary font-bold text-3xl sm:text-4xl">404</h1>
+      <p className=" text-xl sm:text-2xl">Error! Page not found...</p>
     </div>
   );
 }
