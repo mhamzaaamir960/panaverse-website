2cc24d9ba58f109ad54779b4c9c54f8f958f872d
mhamzaaamir960 <mhamzaaamir960.gmail.com>
2024-05-02 22:15:04 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: ["images.ctfassets.net"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.ctfassets.net",
       },
     ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 };
 
