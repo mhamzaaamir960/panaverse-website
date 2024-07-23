fef6c8506a43e6341c56205238d9e4513ff24689
muhammadhamzaaamir960 <137796896+muhammadhamzaaamir960@users.noreply.github.com>
2024-07-23 21:22:11 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
-<<<<<<< HEAD
-=======
-  },
-  experimental: {
-    glob: false,
->>>>>>> a9a2f7505e269d222b125ae12aab6ac970033841
   },
 };
 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 "use client";
 import Head from "next/head";
-function NotFoundPage() {
-  return (
-    <div className="min-h-[85vh] flex flex-col justify-center items-center">
-      <Head>
-        <title>404 - Not Found</title>
-        <meta name="Not Found" content="404 page. Not found" />
-      </Head>
-=======
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -18,11 +9,13 @@ export const metadata: Metadata = {
 function NotFoundPage() {
   return (
     <div className="min-h-[85vh] flex flex-col justify-center items-center">
->>>>>>> a9a2f7505e269d222b125ae12aab6ac970033841
+      <Head>
+        <title>404 - Not Found</title>
+        <meta name="Not Found" content="404 page. Not found" />
+      </Head>
       <h1 className="text-secondary font-bold text-3xl sm:text-4xl">404</h1>
       <p className=" text-xl sm:text-2xl">Error! Page not found...</p>
     </div>
   );
 }
-
 export default NotFoundPage;
